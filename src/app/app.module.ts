44335905fe4ab29d34dc414ac1c34938813489ab
subrata797979 <subrata797979@gmail.com>
2022-04-05 00:57:13 +0530

    skill section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SkillsComponent } from './skill-main/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
