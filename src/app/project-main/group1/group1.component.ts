8a5ab920b6907f2a293de657ede3822f1236d484
subrata797979 <subrata797979@gmail.com>
2022-04-14 01:16:06 +0530

    small tweaks in home section

--- src/app/project-main/group1/group1.component.ts
+++ src/app/project-main/group1/group1.component.ts
@@ -24,6 +24,45 @@ export class Group1Component implements OnInit {
         conclusion!: "A challenging project for understanding REST API calls",
         github!: "https://github.com/subrata797979/12_StudentAPI_CRUD_Backend",
         image!: "../../../assets/projects/group1/springBoot.png"
+      },
+      {
+        pid!: 2,
+        name!: "College Gate Website",
+        start_date!: new Date("2018-01-05"),
+        end_date!: new Date("2018-04-17"),
+        description!: "A website for guiding student to choose best college",
+        type!: "Group",
+        role!: "Frontend and Database Management",
+        technology!: "HTML, CSS, JS, PHP",
+        conclusion!: "A good team work is always the key",
+        github!: "https://github.com/subrata797979/21_CollegeGate",
+        image!: "../../../assets/projects/group1/php.png"
+      },
+      {
+        pid!: 1,
+        name!: "Spring Boot Student CRUD API with Separate Frontend",
+        start_date!: new Date("2022-01-05"),
+        end_date!: new Date("2022-02-12"),
+        description!: "A simple Java Spring Boot Rest API for Create, Read, Delete and Update operations on Students with Separate Frontend",
+        type!: "Individual",
+        role!: "Fullstack Developer, developing from scratch",
+        technology!: "Java Spring Boot Framwork, HTML, CSS, JS, Bootstrap",
+        conclusion!: "A challenging project for understanding REST API calls",
+        github!: "https://github.com/subrata797979/12_StudentAPI_CRUD_Backend",
+        image!: "../../../assets/projects/group1/springBoot.png"
+      },
+      {
+        pid!: 2,
+        name!: "College Gate Website",
+        start_date!: new Date("2018-01-05"),
+        end_date!: new Date("2018-04-17"),
+        description!: "A website for guiding student to choose best college",
+        type!: "Group",
+        role!: "Frontend and Database Management",
+        technology!: "HTML, CSS, JS, PHP",
+        conclusion!: "A good team work is always the key",
+        github!: "https://github.com/subrata797979/21_CollegeGate",
+        image!: "../../../assets/projects/group1/php.png"
       }
     ]
   }
