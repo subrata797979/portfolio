2befe04863a501ef93b0b8cf39a43cfd408cd20b
subrata797979 <subrata797979@gmail.com>
2022-04-28 13:07:25 +0530

    phase 1 final commit

--- src/app/project-main/group2/group2.component.ts
+++ src/app/project-main/group2/group2.component.ts
@@ -14,6 +14,19 @@ export class Group2Component implements OnInit {
     this.group2_projects = [
       {
         pid!: 1,
+        name!: "Simple Portfolio with Modular Components",
+        start_date!: new Date("2022-02-10"),
+        end_date!: new Date(),
+        description!: "A modern porfolio with modular approach in angular",
+        type!: "Individual",
+        role!: "Design, Development",
+        technology!: "Design pattern, Html, CSS, TS, Angular",
+        conclusion!: "A dynamic and continuous developing project and very interesting to do",
+        github!: "https://github.com/subrata797979/portfolio",
+        image!: "../../../assets/home/home.gif"
+      },
+      {
+        pid!: 2,
         name!: "Angular Todo List",
         start_date!: new Date("2020-05-05"),
         end_date!: new Date("2020-07-17"),
@@ -26,7 +39,7 @@ export class Group2Component implements OnInit {
         image!: "../../../assets/projects/group1/angular.png"
       },
       {
-        pid!: 2,
+        pid!: 3,
         name!: "Angular Date Store App (Birthday Reminder)",
         start_date!: new Date("2021-07-11"),
         end_date!: new Date("2020-08-24"),
@@ -39,7 +52,7 @@ export class Group2Component implements OnInit {
         image!: "../../../assets/projects/group1/angular.png"
       },
       {
-        pid!: 3,
+        pid!: 4,
         name!: "Java EE App (1. DateStore  GUI, 2. Calculator) using JDBC",
         start_date!: new Date("2021-08-02"),
         end_date!: new Date("2021-08-21"),
@@ -51,6 +64,7 @@ export class Group2Component implements OnInit {
         github!: "https://github.com/subrata797979/JavaDesktop/tree/main/JavaSwing",
         image!: "../../../assets/projects/group2/java.png"
       }
+      
     ]
   }
 
